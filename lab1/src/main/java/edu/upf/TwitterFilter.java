d263c914cb21cd25d07363e303bcc03106c3363a
zvizzct <victor.puerta01@estudiant.upf.edu>
2023-02-04 23:44:17 +0100

    added the uploader class and trying to execute it

--- lab1/src/main/java/edu/upf/TwitterFilter.java
+++ lab1/src/main/java/edu/upf/TwitterFilter.java
@@ -1,4 +1,3 @@
-
 package edu.upf;
 
 import java.io.IOException;
@@ -6,9 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import edu.upf.filter.FileLanguageFilter;
+import edu.upf.uploader.S3Uploader;
 
 public class TwitterFilter {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         List<String> argsList = Arrays.asList(args);
         String language = argsList.get(0);
         String outputFile = argsList.get(1);
@@ -18,16 +18,23 @@ public class TwitterFilter {
         for (String inputFile : argsList.subList(3, argsList.size())) {
 
             System.out.println("Processing: " + inputFile);
-            final FileLanguageFilter filter = new FileLanguageFilter(inputFile,
-                    outputFile);
             try {
+                final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
                 filter.filterLanguage(language);
             } catch (Exception e) {
-                System.err.println("ERROR:" + e.getMessage());
+                System.err.println("ERROR filtering the file: " + e.getMessage());
             }
         }
 
-        // final S3Uploader uploader = new S3Uploader(bucket, "prefix", "upf");
-        // uploader.upload(Arrays.asList(outputFile));
+        try {
+            final S3Uploader uploader = new S3Uploader(bucket, "lab1", "default");
+            uploader.upload(Arrays.asList(outputFile));
+        } catch (Exception e) {
+            System.err.println("ERROR uploading to S3: " + e.getMessage());
+        }
     }
+    // java -cp
+    // target/classes:~/.m2/repository/junit/junit/4.11/junit-4.11.jar:~/.m2/repository/com/amazonaws/aws-java-sdk-s3/1.12.399/aws-java-sdk-s3-1.12.399.jar:~/.m2/repository/org/jsoup/jsoup/1.13.1/jsoup-1.13.1.jar:~/.m2/repository/com/google/code/gson/gson/2.10.1/gson-2.10.1.jar
+    // edu.upf.TwitterFilter
+
 }
